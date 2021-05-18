2a457aa9464b2b364f42530edfc541434545b272
Csáki Gergő <33156423+Csaki95@users.noreply.github.com>
2021-05-18 10:46:53 +0200

    Removed footer link
    - linkedin throws error in console on their page when my profile is opened by link from another site
    
    This error was reported to them many times, but still haven't fixed it so I removed it all together as it wasn't required.

--- src/app/components/common/footer/footer.component.ts
+++ src/app/components/common/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 /**
  * Footer component with links to github and linkedIn pages
@@ -10,9 +10,7 @@ import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-  linkedinIcon = faLinkedin;
   gitIcon = faGithub;
-  linkedin = 'https://www.linkedin.com/in/gergo-csaki-60b838145';
   github = 'https://github.com/Csaki95/fhir-location-vaccination.git';
 
   constructor() {}
